61006dbf7b84fe590c908bc31ec46b809be13f83
Zhizhong Yan <A2564011261@163.com>
2024-09-17 22:28:17 +0800

    add search

---
+++ js/tab.js
@@ -0,0 +1,5 @@
+function showSearchTool() {
+    document.getElementById("searchTool").style.display = "block";
+    document.getElementById("searchTool").style.opacity = "1";
+    document.getElementById("searchTool").style.transition = "all 0.5s";
+}
