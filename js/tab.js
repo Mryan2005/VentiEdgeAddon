533190762c919cede3c4df776b4d39eceee480ee
Zhizhong Yan <A2564011261@163.com>
2024-09-21 11:35:05 +0800

    fix the problem: cannot display time

--- js/tab.js
+++ js/tab.js
@@ -1,5 +1,23 @@
-function showSearchTool() {
-    document.getElementById("searchTool").style.display = "block";
-    document.getElementById("searchTool").style.opacity = "1";
-    document.getElementById("searchTool").style.transition = "all 0.5s";
-}
+document.getElementById('body1').addEventListener('click', (event) => {
+    const element = document.documentElement; // 获取整个文档的元素
+    if (element.requestFullscreen) { // 标准写法
+        element.requestFullscreen();
+    } else if (element.mozRequestFullScreen) { // Firefox 浏览器
+        element.mozRequestFullScreen();
+    } else if (element.webkitRequestFullscreen) { // Chrome 和 Safari
+        element.webkitRequestFullscreen();
+    } else if (element.msRequestFullscreen) { // IE11
+        element.msRequestFullscreen();
+    }
+});
+
+window.addEventListener('load', (event) => {
+    var time = new Date();
+    var year = time.getFullYear();
+    var month = time.getMonth() + 1;
+    var date = time.getDate();
+    var hour = time.getHours();
+    var minute = time.getMinutes();
+    var timeString = year + '年' + month + '月' + date + '日 ' + hour + ':' + minute;
+    document.getElementById('appleTime').innerHTML = timeString;
+});
