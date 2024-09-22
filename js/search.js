000997a76b4745af2b49aa4b5af709837c9f88fa
Zhizhong Yan <A2564011261@163.com>
2024-09-22 08:52:00 +0800

    updata search function

---
+++ js/search.js
@@ -0,0 +1,13 @@
+document.getElementById('searchBtn').addEventListener('click', (event) => {
+    var searchEngine = document.getElementById('searchEngine').value;
+    var searchWord = document.getElementById('searchWord').value;
+    if(searchEngine === 'google') {
+        var searchUrl = 'https://www.google.com/search?q=';
+    } else if(searchEngine === 'bing') {
+        var searchUrl = 'https://www.bing.com/search?q=';
+    } else if(searchEngine === 'baidu') {
+        var searchUrl = 'https://www.baidu.com/s?wd=';
+    }
+    var searchUrl = searchUrl + searchWord;
+    window.open(searchUrl, '_blank');
+});
