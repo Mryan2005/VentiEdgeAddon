4e6eef67897fc396734709f16d56634bf0e2d70f
Zhizhong Yan <A2564011261@163.com>
2024-09-22 11:06:17 +0800

    add a new function

--- js/tab.js
+++ js/tab.js
@@ -1,3 +1,8 @@
+var stack = ["Finder"];
+var containerStyle = document.getElementById('container').style;
+var topbarStyle = document.getElementById('topbar').style;
+var searchWindowShowStatus, aboutWindowShowStatus;
+
 document.getElementById('body1').addEventListener('click', (event) => {
     const element = document.documentElement; // 获取整个文档的元素
     if (element.requestFullscreen) { // 标准写法
@@ -48,35 +53,69 @@ window.addEventListener('load', (event) => {
 });
 
 document.getElementById('about').addEventListener('click', (event) => {
+    if(stack[stack.length - 1] === 'about') {
+        return;
+    }
+    stack.push('about');
     document.getElementById('aboutWindow').style.display = 'block';
+    document.getElementById('aboutWindow').style.zIndex = stack.length;
     document.getElementById('barTitle').innerHTML = 'About';
 });
 
 document.getElementById('closeAboutWindow').addEventListener('click', (event) => {
+    stack.pop();
     document.getElementById('aboutWindow').style.display = 'none';
     document.getElementById('barTitle').innerHTML = 'Finder';
 });
 
 document.getElementById('closeSearchWindow').addEventListener('click', (event) => {
+    stack.pop();
     document.getElementById('searchWindow').style.display = 'none';
     document.getElementById('barTitle').innerHTML = 'Finder';
 });
 
 document.getElementById('appleSearch').addEventListener('click', (event) => {
+    if(stack[stack.length - 1] === 'search') {
+        return;
+    }
+    stack.push('search');
     document.getElementById('searchWindow').style.display = 'block';
+    document.getElementById('searchWindow').style.zIndex = stack.length;
     document.getElementById('barTitle').innerHTML = 'Search';
 });
 
-document.getElementById('closeDeepLWindow').addEventListener('click', (event) => {
-    document.getElementById('deepLWindow').style.display = 'none';
-    document.getElementById('barTitle').innerHTML = 'Finder';
+document.getElementById('launchpad').addEventListener('click', (event) => {
+    stack.push('launchpad');
+    searchWindowShowStatus = document.getElementById('searchWindow').style.display != 'none' ? document.getElementById('searchWindow').style.display : 'none';
+    aboutWindowShowStatus = document.getElementById('aboutWindow').style.display != 'none' ? document.getElementById('aboutWindow').style.display : 'none';
+    document.getElementById('applications').style.display = 'block';
+    document.getElementById('topbar').style.display = 'none';
+    document.getElementById('container').style.display = 'none';
+    document.getElementById('searchWindow').style.display = 'none';
+    document.getElementById('aboutWindow').style.display = 'none';
 });
 
-document.getElementById('deepLWindowButton').addEventListener('click', (event) => {
-    document.getElementById('deepLWindow').style.display = 'block';
-    document.getElementById('barTitle').innerHTML = 'DeepL';
-});
-
-document.getElementById('deepLWindow').addEventListener('load', (event) => {
-    window.open('https://www.deepl.com/translator', '_self');
+document.addEventListener('DOMContentLoaded', (event) => {
+    navigator.keyboard.lock(['Escape']);
+    document.addEventListener('keydown', function(e) {
+        // console.log(`键名: ${e.key}, 键码: ${e.code}`);
+        if(e.key === 'Escape' && stack.length > 0) {
+            if(stack[stack.length - 1] === 'about') {
+                document.getElementById('aboutWindow').style.display = 'none';
+                document.getElementById('barTitle').innerHTML = stack[stack.length - 2] == 'undefined' ? 'Finder' : stack[stack.length - 2];
+            } else if(stack[stack.length - 1] === 'search') {
+                document.getElementById('searchWindow').style.display = 'none';
+                document.getElementById('barTitle').innerHTML = stack[stack.length - 2] == 'undefined' ? 'Finder' : stack[stack.length - 2];
+            } else if(stack[stack.length - 1] === 'launchpad') {
+                document.getElementById('applications').style.display = 'none';
+                document.getElementById('topbar').style = topbarStyle;
+                document.getElementById('container').style = containerStyle;
+                document.getElementById('searchWindow').style.display = searchWindowShowStatus;
+                document.getElementById('aboutWindow').style.display = aboutWindowShowStatus;
+            } else if(stack[stack.length - 1] === 'Finder') {
+                return;
+            }
+            stack.pop();
+        }
+    });
 });
